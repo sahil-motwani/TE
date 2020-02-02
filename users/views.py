393c739dca370d0937e35f3d2aa237188b1dd5ce
ektakithani <2017.ekta.kithani@ves.ac.in>
2020-02-02 12:59:58 +0530

    Frontendpart

---
+++ users/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm,ProfileRegisterForm
+# Create your views here.
+def register(request):
+    if request.method=='POST':
+        form=UserRegisterForm(request.POST)
+        p_reg_form = ProfileRegisterForm(request.POST)
+        if form.is_valid() and p_reg_form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            p_reg_form = ProfileRegisterForm(request.POST, instance=user.profile)
+            p_reg_form.full_clean()
+            p_reg_form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request, "Your Account has been created ! Answer some questions then you can now login")
+            return redirect('questions')
+    else:
+        form=UserRegisterForm()
+        p_reg_form = ProfileRegisterForm()
+    context = {
+    'form': form,
+    'p_reg_form': p_reg_form
+    }
+    return render(request,'users/register.html',context)
+@login_required
+def profile(request):
+    if request.method=='POST':
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Your Account has been updated !")
+            return redirect('profile')
+
+    else:
+        u_form=UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForm(instance=request.user.profile)
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'users/profile.html',context)
+
+def questions(request):
+    return redirect('questions')
